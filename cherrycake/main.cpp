7132695bb29373dad9bf4cd0c63adf9fd6e2654f
William McGonagle <wpmcgonagle@gmail.com>
2022-11-30 10:49:09 -0500

    Initial Commit

---
+++ cherrycake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cherrycake
+//
+//  Created by William McGonagle on 11/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
