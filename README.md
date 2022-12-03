def15f430e0ee3ea1ccda534f166bd0b325ad9b2
William McGonagle <wpmcgonagle@gmail.com>
2022-12-03 16:05:17 -0500

    Please Fix These Git Issues

---
+++ README.md
@@ -0,0 +1,44 @@
+# The Cherry Cake CLI
+
+The CLI for a server-focused, interpreted and compiled programming language with simplicity, speed, and size in mind.
+
+## Installation
+
+You can install Cherry Cake by downloading any of the executables on the releases page. If there is not an executable for your platform, please make sure to create an issue on Github the name of your platform. 
+
+If you are able to create an installation shortcut for Cherry Cake, please let us know by creating an issue and we will add it to the readme.
+
+## Examples
+
+### Hello World (`hello_world.chry`)
+
+The 'hello world' example is the most basic http-program that you can make in Cherry Cake. If you run this following code and visit [localhost:3000](http://localhost:3000/), you will see "Hello World".
+
+```cherry
+
+# Code inside runs when you go to '/'
+get ~/ {
+
+    # Responds with "Hello World"
+    res.send("Hello World")
+
+}
+
+```
+
+### Top-level Example (`top_level.chry`)
+
+Top-level means that code in Cherry Cake does not need to be inside of a 'main' or 'start' function. This makes Cherry Cake a bit more flexible and simple than some traditional languages because it reduces the need to boilerplate code. But, Top-level programming can still be a bit tricky in Cherry Cake.
+The following code will run immediately since there are no server endpoints in the below code. If there were endpoints, the code would wait for the server to initialize and then run the REPL code after. 
+
+```cherry
+
+# REPL Code runs immediately after the server is initialized
+console.log("Hello World");
+console.log(7 * 6); # prints 42
+
+```
+
+## License
+
+The code in this project (the compiler, interpretter, tester and all other tools packaged with 'The Cherry Cake CLI') is licensed under the BSD four-clause license. The language itself (syntax, tokens, api, etc) is publicly available for general use, as all languages are. That being said, [The Fairfield Programming Association Inc.](https://about.fairfieldprogramming.org/) is the Benevolent Dictator for Life ([BDFL](https://en.wikipedia.org/wiki/Benevolent_dictator_for_life)), and manages a number of specifications to keep the language standardized across all implementations.  
---
+++ src/token_triggers.h
@@ -0,0 +1,133 @@
+//
+//  tokenizer.h
+//  cherrycake
+//
+//  Created by William McGonagle on 11/30/22.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#pragma once
+
+// Struct for defining each scope
+struct Scope
+{
+
+    std::string name;
+
+    std::string start;
+    std::string end;
+
+    std::string initiator;
+    std::string conditional;
+
+    bool uses_start;
+
+    Scope(std::string _name, std::string a, std::string b, bool _uses_start)
+    {
+
+        name = _name;
+        uses_start = _uses_start;
+
+        if (_uses_start)
+        {
+
+            start = a;
+            end = b;
+        }
+        else
+        {
+
+            initiator = a;
+            conditional = b;
+        }
+    }
+};
+
+// Struct for defining each symbol
+struct Symbol
+{
+
+    std::string name;
+    std::string data;
+
+    Symbol(std::string _name, std::string _data)
+    {
+
+        name = _name;
+        data = _data;
+    }
+};
+
+// Define all posible keywords that the language can support
+const std::string keywords[] = {
+    // Defining Functions
+    "func",
+
+    // Conditional Logic Package
+    "if",
+    "else",
+    "true",
+    "false",
+
+    // Looping Package
+    "for",
+
+    // Logical Flow Package
+    "break",
+    "continue",
+    "in",
+
+    // Null Data
+    "null",
+
+    // HTTP Keywords
+    "manifest",
+    "funnel",
+    "get",
+    "post",
+    "put",
+    "delete",
+    "cached",
+    "mined",
+
+    // Data Types
+    "int",
+    "float",
+    "bool",
+    "string"};
+
+// Define all posible symbols that the language can support
+const Symbol symbols[] = {
+    Symbol("INCREASE_SCOPE", "{"),
+    Symbol("DECREASE_SCOPE", "}"),
+    Symbol("EQUALS", "=="),
+    Symbol("GREATER_THAN_OR_EQUALS", ">="),
+    Symbol("LESS_THAN_OR_EQUALS", "<="),
+    Symbol("GREATER_THAN", ">"),
+    Symbol("LESS_THAN", "<"),
+    Symbol("SET_KEYWORD", "="),
+    Symbol("START_PAREN", "("),
+    Symbol("END_PAREN", ")"),
+    Symbol("CHAIN", "."),
+    Symbol("PARAM_SEPARATOR", ","),
+    Symbol("LOGICAL_AND", "&&"),
+    Symbol("LOGICAL_OR", "||"),
+    Symbol("LOGICAL_XOR", "^^"),
+    Symbol("ADD_OPERATOR", "+"),
+    Symbol("SUB_OPERATOR", "-"),
+    Symbol("DIV_OPERATOR", "/"),
+    Symbol("MUL_OPERATOR", "*"),
+    Symbol("MOD_OPERATOR", "%")};
+
+// Define all of the possible scopes in the programming language
+const Scope scopes[] = {
+    Scope("STRING", "\"", "\"", true),
+    Scope("LINE_COMMENT", "#", "\n", true),
+    Scope("IDENTIFIER", "[_a-zA-Z]", "[_a-zA-Z0-9]", false),
+    Scope("NUMERICAL", "[0-9]", "[0-9\\.]", false),
+    Scope("URL", "[\\~]", "[\\/:_\\-0-9a-zA-Z]", false),
+};
